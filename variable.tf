46dda837bf8f901f44cc94816c423a9e931adca0
CloudSihmar <105888131+CloudSihmar@users.noreply.github.com>
2023-06-08 00:41:22 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  description = "Region of AWS"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 Instance Type "
+  type        = string
+}
+
+variable "ami" {
+  description = "Region of AWS"
+  type        = string
+}
